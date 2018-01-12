1df2c0435e73a35ee97eeff64f49de3891bd3fe7
eblanch202 <hellspurge666@gmail.com>
2018-01-12 17:47:57 +1100

    Templatised the index and tested using template engine

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import backend_objects
+import templater
 from tornado.ncss import Server, ncssbook_log
 
 TEMPLATE_DIR = "templates"
@@ -8,11 +10,14 @@ def get_template(filename):
         return f.read()
 
 def home_page_handler(request):
-    request.write(get_template("index.html"))
+    #charity = backend_objects.getRandomCharity()
+    charity = backend_objects.Charity("Snail Helpline", "We help snails!!")
+    context = {"charity": charity}
+    request.write(templater.render("templates/index.html", context))
 
 def charity_profile_handler(request, charity_profile_id):
     #request.write("Here is the profile for charity " + charity_profile_id + ".")
-    request.write(get_template("charity.html").format(charity_profile_id = charity_profile_id))
+    request.write(get_template("charity.html").format(charity_profile_id = charity_profile_id, charity_name = "charity_name", charity_logo = "charity_logo"))
 
 def create_charity_profile_handler(request):
     pass
@@ -41,7 +46,7 @@ def default_handler(request, method):
 """ Sends user to new page and sends info to data base"""
 
 server = Server()
-server.register(r"/", home_page_handler)
+server.register(r"/?", home_page_handler)
 server.register(r"/charity_profile/(\d+)/?", charity_profile_handler)
 server.register(r"/create_charity_profile/?", create_charity_profile_handler)
 server.register(r"/post_create_profile/(.+)/(.+)/?", post_create_profile_handler)
