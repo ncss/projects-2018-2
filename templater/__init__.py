d92a835bc7b6ae91fdd13fbc6474c7972c9c1f11
Shourov Quazi <qshourov@gmail.com>
2018-01-12 16:21:19 +1100

    MVP ___Thomas_The_Template_Engine___ finished
    Jackson, Ethan, Ollie, Shovel are da best

---
+++ templater/__init__.py
@@ -0,0 +1,23 @@
+"""
+___Thomas_The_Template_Engine___
+
+      __        __________
+     /  \         ========   _____________
+      ||          =      =  /           ]
+  ___==============      = /            ]
+  \_[            ========= [            ]
+    [=====================^==============
+___//_(_)_(_)_(_)___\__/_____(_)_(_)_(_)
+========================================
+
+Syntax:
+
+
+"""
+
+from node_parser import *
+
+def render(file,context):
+    '''Renders given html file'''
+    with open(file) as f:
+        return parse(f.read()).translate(context)
