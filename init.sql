f5831c14a2e0df024ddd1b333a0436a35ca9b4d8
purple-waves <rachel.schroder@bigpond.com>
2018-01-12 12:32:18 +1100

    initial database schema for charity app
    Implements users, charity, charity_followers and friends tables
    Includes some fake data

--- init.sql
+++ init.sql
@@ -1,50 +1,47 @@
-CREATE TABLE people (
+CREATE TABLE users (
   id INTEGER NOT NULL,
-  fname TEXT NOT NULL,
-  lname TEXT NOT NULL,
-  gender TEXT NOT NULL,
-  age INTEGER NOT NULL,
+  username TEXT NOT NULL,
+  pword TEXT NOT NULL,
+  fname TEXT,
+  sname TEXT,
+  email TEXT NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE events (
+CREATE TABLE charity (
   id INTEGER NOT NULL,
   name TEXT NOT NULL,
-  age INTEGER NOT NULL,
-  gender TEXT NOT NULL,
-  at TEXT NOT NULL,
+  category TEXT,
+  story TEXT,
+  charity_website_url TEXT,
+  image_src TEXT,
+  admin_id INTEGER, 
+  FOREIGN KEY (admin_id) REFERENCES users (id),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE results (
-  event INTEGER NOT NULL,
-  person INTEGER NOT NULL,
-  result TEXT,
-  PRIMARY KEY (event, person),
-  FOREIGN KEY (event) REFERENCES events (id),
-  FOREIGN KEY (person) REFERENCES people (id)
+CREATE TABLE charity_followers (
+  charity_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (charity_id) REFERENCES charity (id),
+  PRIMARY KEY (user_id,charity_id)
 );
 
-INSERT INTO people VALUES (      0, 'Barry', 'Schultz', 'M', 16);
-INSERT INTO people VALUES (      1, 'Prue', 'Robinson', 'F', 17 );
-INSERT INTO people VALUES (      2, 'Andrew', 'Varvel', 'M', 16 );
-INSERT INTO people VALUES (      3, 'Mathew', 'Nemes', 'M', 13 );
-INSERT INTO people VALUES (      4, 'Mara', 'Barber', 'F', 17 );
-INSERT INTO people VALUES (      5, 'Scott', 'Herdman', 'M', 16 );
-INSERT INTO people VALUES (      6, 'Alec', 'Newton', 'M', 16 );
-INSERT INTO people VALUES (      7, 'Karen', 'Barber', 'F', 14 );
-INSERT INTO people VALUES (      8, 'Grant', 'Ovzinsky', 'M', 17 );
+CREATE TABLE friends (
+  user_id1 INTEGER NOT NULL,
+  user_id2 INTEGER NOT NULL,
+  FOREIGN KEY (user_id1) REFERENCES users (id),
+  FOREIGN KEY (user_id2) REFERENCES users (id),
+  PRIMARY KEY (user_id1, user_id2)
+);
 
-INSERT INTO events VALUES (      0, '100m', 16, 'M', '09:10' );
-INSERT INTO events VALUES (      1, '200m', 16, 'M', '09:15' );
-INSERT INTO events VALUES (      2, '100m', 17, 'M', '09:00' );
-INSERT INTO events VALUES (      3, '100m', 17, 'F', '09:05' );
 
-INSERT INTO results VALUES (      0, 5, '00:21');
-INSERT INTO results VALUES (      0, 0, '00:15');
-INSERT INTO results VALUES (      3, 1, '00:20');
-INSERT INTO results VALUES (      0, 2, '00:17');
-INSERT INTO results VALUES (      0, 6, NULL);
-INSERT INTO results VALUES (      3, 4, '00:20');
-INSERT INTO results VALUES (      2, 8, NULL);
-INSERT INTO results VALUES (      1, 0, '00:40');
+/* FAKE DATA */
+INSERT INTO users(id,username,pword,email) VALUES (0, 'foo', 'bar', 'baz@snailmail.com');
+INSERT INTO users(id,username,pword,email) VALUES (1, 'bob', 'abc', 'bob@snailmail.com');
+INSERT INTO users(id,username,pword,email) VALUES (2, 'fred', '1234', 'george@snailmail.com');
+INSERT INTO users(id,username,pword,email) VALUES (3, 'percy', 'trident', 'riptide@snailmail.com');
+
+INSERT INTO charity(id, name, story) VALUES (4, 'snail helpline', 'we help sad snails');
+INSERT INTO charity(id, name, category, admin_id) VALUES (5, 'abc', 'we help sad snails',1);
