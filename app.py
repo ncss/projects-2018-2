cfc215904fd5f9613c142a1c014fe1c8c58dbfa4
eblanch202 <hellspurge666@gmail.com>
2018-01-12 11:59:40 +1100

    Got the handler working with the style guide

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from tornado.ncss import Server, ncssbook_log
 
+def home_page_handler(request):
+	request.write(open("style_guide/index.html").read())
+
 def charity_profile_handler(request, profile_id):
 	request.write("Here is the profile for charity " + profile_id + ".")
-	
+
 def create_charity_profile_handler(request):
 	pass
 	"""Wanting to do something here but not sure what yet."""
-	
+
 def post_create_profile_handler(request, charity_name, charity_logo):
 	request.write("You have added a Charity with this name " + charity_name + " and logo " + charity_logo)
-	
+
 def swipe_screen_handler(request, profile_id, swipe_direction):
 	request.write("You swiped " + swipe_direction + " for the Charity " + profile_id)
-	
+
 """def next_charity_handler(request, )"""
 """ Sends user to new page and sends info to data base"""
 
 server = Server()
+server.register(r"/", home_page_handler)
 server.register(r"/charity_profile/(\d+)/", charity_profile_handler)
 server.register(r"/create_charity_profile/", create_charity_profile_handler)
 server.register(r"/post_create_profile/(.+)/(.+)", post_create_profile_handler)
