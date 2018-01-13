e007e51342bb4b6aa5c7b6d798dc5edffc847e6c
Ethan Hansen <ethoshansen@gmail.com>
2018-01-14 02:11:39 +1100

    Added initial swipe functionality.
    See above.

---
+++ recommender.py
@@ -0,0 +1,10 @@
+import backend_objects
+
+snailLovers = backend_objects.Charity("Give A Snail A Home", "", "", "", 3, ["snail", "snails", "animals", "animal welfare", "homeless"])
+snailLovers = backend_objects.Charity("Snail Protection Agency", "", "", "", 4, ["snail", "snails", "animals", "animal welfare", "protection", "safety"])
+
+
+print()
+
+
+    
--- static/main.js
+++ static/main.js
@@ -17,3 +17,65 @@ leftarrow.addEventListener("click", (ev) => {
   }, 1000);
   return false;
 });
+
+let startPosition = null;
+let click = false;
+
+charitylogo.draggable = false;
+charitylogo.addEventListener("pointerdown", (ev) =>  {
+	startPosition = ev.screenX;
+	click = true;
+	console.log('pdown');
+})
+
+charitylogo.addEventListener("pointerup", (ev) =>  {
+	startPosition = null;
+	charitylogo.style.transform = null;
+	click = false;
+	console.log('pup');
+})
+
+document.body.addEventListener("pointerleave", (ev) =>  {
+	if (ev.pointerType != "touch") {
+	startPosition = null;
+	charitylogo.style.transform = null;
+	click = false;
+	console.log(ev);
+	console.log('pleave');
+	}
+})
+
+document.body.addEventListener("touchend", (ev) => {
+		startPosition = null;
+	charitylogo.style.transform = null;
+	click = false;
+	console.log('pup');
+
+	
+	
+})
+
+charitylogo.addEventListener("pointermove", (ev) =>  {
+	// Needed (brackets) around your if condition and needed to set startPosition to null
+	if (click == true) {
+		console.log(startPosition);
+		let delta = ev.screenX - startPosition;
+		console.log(ev.screenX);
+		let rotation = delta * 0.0174;
+		charitylogo.style.transform = `rotate(${rotation}deg) translate(${delta}px)`;
+		console.log(`rotate(${rotation}deg) translate(${delta}px)`);
+	}
+});
+
+charitylogo.addEventListener("touchmove", (ev) =>  {
+	// Needed (brackets) around your if condition and needed to set startPosition to null
+	if (click == true) {
+		console.log(ev);
+		console.log(startPosition);
+		let delta = ev.touches[0].screenX - startPosition;
+		console.log(ev.touches[0].screenX);
+		let rotation = delta * 0.0174;
+		charitylogo.style.transform = `rotate(${rotation}deg) translate(${delta}px)`;
+		console.log(`rotate(${rotation}deg) translate(${delta}px)`);
+	}
+});
