55f770650fec47e33f2baf551eeb0bc019515dbf
Leanne Chung <leannechung00@gmail.com>
2018-01-14 01:47:50 +1100

    Speed up swiping

--- static/main.js
+++ static/main.js
@@ -4,6 +4,7 @@ rightarrow.addEventListener("click", (ev) => {
   setTimeout(() => {
     charitylogo.classList.remove("swipe_image_left");
     window.location.replace("/");
+  }, 1000);
   return false;
 });
 
@@ -13,5 +14,6 @@ leftarrow.addEventListener("click", (ev) => {
   setTimeout(() => {
     charitylogo.classList.remove("swipe_image_right");
     window.location.replace("/");
+  }, 1000);
   return false;
 });
